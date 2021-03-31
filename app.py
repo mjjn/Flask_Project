e9a117b84bb83cbdda8ebf8ee63d9f99d727ff8f
Aadish Jain <jainaadish192@gmail.com>
2021-03-31 18:04:52 +0530

    second commit

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
+
 if __name__ == '__main__':
     app.run()
